c6867d2ad928eec7d21d3e2c5b597acff3fd82e0
Grissom Huang <greoffer@gmail.com>
2017-02-20 10:40:50 -0800

    Update createfile.rb

--- phpapp/recipes/createfile.rb
+++ phpapp/recipes/createfile.rb
@@ -1,4 +1,9 @@
-file '/srv/www/index2.php' do
+file '/var/www/html/index2.php' do
+  content '<html>This is a placeholder for the home page.</html>'
+  mode 0644
+end
+
+file '/srv/www/simplephpapp/index2.php' do
   content '<html>This is a placeholder for the home page.</html>'
   mode 0644
 end
